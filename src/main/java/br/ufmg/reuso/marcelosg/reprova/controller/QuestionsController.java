39348cacbae7f00e488b5f1374705d9d1cd01453
Marcelo Santos Guimaraes <marcelo.guimaraes@bancointer.com.br>
2020-09-28 20:46:39 -0300

    Some adjustments to question.

--- src/main/java/br/ufmg/reuso/marcelosg/reprova/controller/QuestionsController.java
+++ src/main/java/br/ufmg/reuso/marcelosg/reprova/controller/QuestionsController.java
@@ -45,7 +45,4 @@ public class QuestionsController {
         return question;
     }
 
-
-
-
 }
---
+++ src/main/java/br/ufmg/reuso/marcelosg/reprova/enums/QuestionDifficulty.java
@@ -0,0 +1,9 @@
+package br.ufmg.reuso.marcelosg.reprova.enums;
+
+public enum QuestionDifficulty {
+
+    EASY,
+    MEDIUM,
+    HARD,
+    VERY_HARD
+}
